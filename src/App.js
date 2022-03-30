c60965afeeb9b14d4c6d29b4dcea69d2885f7c40
Mooner92 <chlaudgjs0989@gmail.com>
2022-03-30 10:01:53 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let tyt = {color : 'yellow',fontSize : '30px'}
+  let posts = "강남 고기 맛집"
+  var [a,b] = [10,100];
+  let [a,b] = useState('남자 코트 추천');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={tyt}>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3>{posts}</h3>
+        <p>3월 29일 발행</p>
+        <hr/>
+
+      </div>
     </div>
   );
 }
