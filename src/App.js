b4a8e0db4f5e7beb67917a813f15491ef5a2beed
Mooner92 <chlaudgjs0989@gmail.com>
2022-04-01 11:03:03 +0900

    add

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ function App() {
   let tyt = {color : 'yellow',fontSize : '30px'}
   let posts = "강남 고기 맛집"
   var [a,b] = [10,100];
-  let [a,b] = useState('남자 코트 추천');
+  let [글제목,글제목변경] = useState('남자 코트 추천');
+  //let [글제목,글제목변경] = useState('남자코트추천');
   return (
     <div className="App">
       <div className="black-nav">
         <div style={tyt}>개발 blog</div>
       </div>
       <div className="list">
-        <h3>{posts}</h3>
+        <h3>{글제목}</h3>
         <p>3월 29일 발행</p>
         <hr/>
 
